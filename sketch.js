72ffc7ddc477a21fd4d82f06663688d444916480
Greg Lubin <glubin@wustl.edu>
2015-09-18 22:59:09 -0500

    working countdown

--- sketch.js
+++ sketch.js
@@ -1,19 +1,26 @@
 var climber;
-var ClimbSpeed = -0.25;
+var ClimbSpeed = -0.10;
 var summit;
 var ice;
 var counter = 0;
 var obstacles;
 var initClimb;
+var count = 30;
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
 
 function setup() {
-  createCanvas(800,500);
+  createCanvas(850,500);
   var xStart = random(0,800);
   climber = createSprite(xStart,485, 25, 25);
   initClimb = new Group();
   initClimb.add(climber);
   obstacles = new Group();
-  var xGoal = random(0,800);
+  // var xGoal = random(0,800);
+  startBlock1 = createSprite(430, 120, 100, 15);
+  startBlock2 = createSprite(200, 300, 100, 15);
+  startBlock3 = createSprite(600, 250, 100, 15);
+
+  obstacles.add(startBlock1);
 }
 
 function draw() {
@@ -26,10 +33,12 @@ function draw() {
   if(keyWentDown("a") || keyWentDown(LEFT_ARROW)){climber.velocity.x += ClimbSpeed; counter = counter + 1; ice = new Pain();}
   if(keyWentDown("d") || keyWentDown(RIGHT_ARROW)){climber.velocity.x -= ClimbSpeed; counter = counter + 1; ice = new Pain();}
  
-  if (climber.velocity.y >= 1.5){climber.velocity.y = 0;}
-  if (climber.velocity.y <= -1.5){climber.velocity.y = 0;}
-  if (climber.velocity.x >= 1.5){climber.velocity.x = 0;}
-  if (climber.velocity.x <= -1.5){climber.velocity.x = 0;}
+ // later adjust max speed
+  var maxSpeed = 0.75; 
+  if (climber.velocity.y >= maxSpeed){climber.velocity.y = 0;}
+  if (climber.velocity.y <= -maxSpeed){climber.velocity.y = 0;}
+  if (climber.velocity.x >= maxSpeed){climber.velocity.x = 0;}
+  if (climber.velocity.x <= -maxSpeed){climber.velocity.x = 0;}
   checkWon();
 
   for (i=0; i<obstacles.length; i++){
@@ -45,7 +54,7 @@ function draw() {
 function checkWon(){
 	if (counter >= 10){
 		alert("you lose!");
-    		location.reload();
+    location.reload();
 	}
 	if (climber.position.y < 60 & climber.position.x > 410 & climber.position.x < 440){
 		alert("you win!");
@@ -60,4 +69,21 @@ function Pain() {
 	this.y = random(0,400);
 	newSprite = createSprite(this.x, this.y, this.width, this.height);
   obstacles.add(newSprite);
+}
+
+
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+    alert("done!");
+    location.reload();
+  }
+  displayTimer();
+}
+
+
+function displayTimer(){
+  document.getElementById("showCounter").innerHTML=count + " secs";
 }
